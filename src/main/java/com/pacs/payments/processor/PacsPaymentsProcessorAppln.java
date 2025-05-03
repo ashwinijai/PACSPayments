2c6e32224b7b8f5538dc2af760a4c7e261fad11f
ashwinijayaraman <ashwinijai@gmail.com>
2025-05-03 23:07:49 +0530

    Mongo, H2 configuration setup

---
+++ src/main/java/com/pacs/payments/processor/PacsPaymentsProcessorAppln.java
@@ -0,0 +1,11 @@
+package com.pacs.payments.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacsPaymentsProcessorAppln {
+    public static void main(String[] args){
+        SpringApplication.run(PacsPaymentsProcessorAppln.class, args);
+    }
+}
